554ca9c2b97273856a4b7444f3118e239253f897
Sage Liem <sage.liem.9@gmail.com>
2025-10-09 08:39:52 -0500

    change data structure and add serialization

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use html2text::from_read;
+use ratatui::{crossterm::terminal, widgets::ListState};
 use rss::{Channel, Item};
 use std::{
     error::Error,
@@ -6,7 +8,7 @@ use std::{
     io::{BufReader, Write},
     path::Path,
 };
-use ratatui::widgets::ListState;
+use serde::{Serializer, Deserializer, Deserialize, Serialize};
 
 pub enum Screen {
     Reader,
@@ -15,79 +17,174 @@ pub enum Screen {
     Exiting,
 }
 
-impl Default for Screen {
-    fn default() -> Screen {
-        Screen::MainMenu
+#[derive(Serialize, Deserialize)]
+pub struct Index {
+    pub meta: Vec<Metadata>,
+    pub state: ListState,
+}
+
+impl Index {
+    pub fn save(&self) -> Result<(), Box<dyn Error>> {
+        let serialized = serde_json::to_string(&self).unwrap();
+        let path = Path::new("feeds/index.json");
+        let mut file = File::create(path)?;
+        file.write_all(serialized.as_ref())?;
+
+        Ok(())
+    }
+
+    pub fn from_file() -> Result<Index, Box<dyn Error>> {
+        let path = Path::new("feeds/index.json");
+        let content = fs::read_to_string(path)?;
+        Ok(serde_json::from_str(&content)?)
     }
 }
 
-pub struct ChannelList {
-    pub items: Vec<Feed>,
-    pub state: ListState,
-    pub currently_viewing: Option<usize>,
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Metadata {
+    pub id: String,
+    pub title: String,
+    pub url: String,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Feed {
-    pub channel: Channel,
+    pub meta: Metadata,
+    pub posts: Vec<Post>,
     pub state: ListState,
-    pub currently_viewing: Option<usize>,
 }
 
 impl Feed {
-    pub fn from(channel: Channel) -> Feed {
-        Feed {
-            channel: channel,
-            state: ListState::default().with_selected(Some(0)),
-            currently_viewing: None,
+    pub fn from_channel(channel: Channel) -> Result<Feed, Box<dyn Error>> {
+        let mut posts = Vec::new();
+        for item in channel.items() {
+            posts.push(Post::from_item(item)?);
         }
+
+        let mut hasher = DefaultHasher::new();
+        channel.link().hash(&mut hasher);
+        Ok(Feed {
+            meta: Metadata {
+                id: hasher.finish().to_string(),
+                title: channel.title().to_string(),
+                url: channel.link().to_string(),
+            },
+            posts: posts,
+            state: ListState::default().with_selected(Some(0)),
+        })
+    }
+
+    pub fn from_file(feed_id: String) -> Result<Feed, Box<dyn Error>> {
+        let path = Path::new("feeds/").join(feed_id);
+        let content = fs::read_to_string(path)?;
+        Ok(serde_json::from_str(&content)?)
+    }
+
+    pub fn save(&self) -> Result<(), Box<dyn Error>> {
+        let serialized = serde_json::to_string(&self).unwrap();
+
+        let mut hasher = DefaultHasher::new();
+        self.meta.url.hash(&mut hasher);
+        let path = Path::new("feeds/").join(hasher.finish().to_string());
+        if !path.exists() {
+            let mut file = File::create(path)?;
+            file.write_all(serialized.as_ref())?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Post {
+    pub title: String,
+    pub content: String,
+    pub scroll: (u16, u16),
+}
+
+impl Post {
+    pub fn from_item(item: &Item) -> Result<Post, Box<dyn Error>> {
+        let title = match item.title() {
+            Some(t) => t.to_string(),
+            None => "title not found".to_string(),
+        };
+        let html_content = match item.content() {
+            Some(t) => t.as_bytes(),
+            None => "Could not get content from post.".as_bytes(),
+        };
+        let text_content = match from_read(html_content, usize::from(terminal::size().unwrap().0)) {
+            Ok(t) => t,
+            Err(e) => e.to_string(),
+        };
+
+        Ok(Post {
+            title: title,
+            content: text_content,
+            scroll: (0, 0),
+        })
+    }
+    pub fn scroll_up(&mut self) {
+        if (self.scroll.0 > 0) {
+            self.scroll.0 -= 1
+        };
+    }
+    pub fn scroll_down(&mut self) {
+        self.scroll.0 += 1
     }
 }
 
 pub struct App {
     pub current_screen: Screen,
-    pub channels: ChannelList,
+    pub index: Index,
+    pub feeds: Vec<Feed>,
 }
 
 impl App {
     pub fn new() -> App {
         App {
-            current_screen: Screen::default(),
-            channels: ChannelList {
-                items: Vec::new(),
+            current_screen: Screen::MainMenu,
+            index: Index {
+                meta: Vec::new(),
                 state: ListState::default().with_selected(Some(0)),
-                currently_viewing: None,
             },
+            feeds: Vec::new(),
         }
     }
 
-    fn make_feed_dir() -> Result<(), Box<dyn Error>> {
-        fs::create_dir("feeds")?;
-        Ok(())
+    pub fn save_index(&self) {
+        self.index.save();
     }
 
-    pub fn init(&mut self) -> Result<(), Box<dyn Error>> {
-        _ = App::make_feed_dir();
+    pub fn load_all(&mut self) -> Result<(), Box<dyn Error>> {
+        _ = fs::create_dir("feeds");
+        self.index = Index::from_file()?;
 
-        for feed in fs::read_dir(&Path::new("feeds/"))? {
-            let feed = File::open(feed?.path())?;
-            let channel = Channel::read_from(BufReader::new(feed))?;
-            self.channels.items.push(Feed::from(channel));
-        }
+        self.index.meta.iter().for_each(|meta| {
+            self.feeds.push(Feed::from_file(meta.id.clone()).unwrap());
+        });
 
         Ok(())
     }
 
     pub async fn add_channel(&mut self, url: &str) -> Result<(), Box<dyn Error>> {
-        _ = App::make_feed_dir();
+        // check if channel already exists
+        // for m in &self.index.meta {
+        //     if m.url == url.to_string() {
+        //         return Ok(());
+        //     }
+        // }
 
-        let mut hasher = DefaultHasher::new();
-        url.hash(&mut hasher);
-        let path = Path::new("feeds/").join(hasher.finish().to_string());
-        if !path.exists() {
-            let content = reqwest::get(url).await?.bytes().await?;
-            let mut file = File::create(path)?;
-            file.write_all(content.as_ref())?;
-        }
+        let content = reqwest::get(url).await?.bytes().await?;
+        let channel = Channel::read_from(&content[..])?;
+        let feed = Feed::from_channel(channel)?;
+        self.index.meta.push(feed.meta.clone());
+        feed.save();
+        self.save_index();
         Ok(())
     }
+}
+
+fn create_feed_dir() -> Result<(), Box<dyn Error>> {
+    _ = fs::create_dir("feeds")?;
+    Ok(())
 }
